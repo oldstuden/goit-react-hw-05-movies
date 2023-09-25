cb5400ef711616a55654e2bd6d84434396ffd521
oldstuden <atikin3002@gmail.com>
2023-09-25 20:48:53 +0200

    used to Navigate

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from '../Layout/Layout';
 import { lazy } from 'react';
 
@@ -19,7 +19,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />}></Route>
           <Route path="reviews" element={<Reviews />}></Route>
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
   );
